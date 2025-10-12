b6fbcc71285779e2c20b4045f6ac0730a4213711
Ricardo <zontriger@gmail.com>
2025-10-12 12:12:19 -0400

    Cambio a Tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [
+      tailwindcss({
+        config: {
+          content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+        },
+      }),
+    ],
+  },
 });
